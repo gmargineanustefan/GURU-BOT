88715ce0cbd376598a7e3ca3920f5ef4ced839af
gmargineanustefan <100882976+gmargineanustefan@users.noreply.github.com>
2024-03-18 19:23:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
-  [''] 
+  ['+35795703518', 'king👑', true],
+  ['+357 95 171357', 'Stefam', true], 
+  ['2'] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['+35795703518','+357 95 171357'] 
+global.prems = ['+35795703518', '+357 95 171357', '']
+global.allowed = ['+35795703518', '+357 95 171357', '']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
@@ -50,18 +50,17 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'king bot👑'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
-global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
-global.dygp = 'https://chat.whatsapp.com/BFfD1C0mTDDDfVdKPkxRAA'
-global.fgsc = 'https://github.com/Guru322/GURU-BOT' 
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
-global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg' 
-global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
+global.author = '@_m_s_gabriel' 
+global.menuvid =''
+global.igfg = '▢ Follow on Instagram\nhttps://https://www.instagram.com/m_s_gabriel?igsh=MXdlbHRkYnd4cDJwMA==\n' 
+global.dygp = 'https://chat.whatsapp.com/DztE3YHnlqxJ8mWJdugfkq'' 
+global.fgyt = ''
+global.fgpyp = ''
+global.fglog = '' 
+global.thumb = fs.readFileSync('')
 
 
 global.wait = '*⌛ _Charging..._*\n*▰▰▰▱▱▱▱▱*'
